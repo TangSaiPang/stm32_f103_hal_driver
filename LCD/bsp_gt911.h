85f508c87f904c424ae13da23cd7e1af978d2934
TangSaiPang <89617575+TangSaiPang@users.noreply.github.com>
2024-06-25 22:27:10 +0800

    Update bsp_gt911.h

--- LCD/bsp_gt911.h
+++ LCD/bsp_gt911.h
@@ -1,13 +1,13 @@
 #ifndef __BSP_GT911_H_
 #define __BSP_GT911_H_
 
-#include "CTIIC.h"
+#include "i2c.h"
 
 
 #define TOUCH_MAX 5
 
 #define GT_WR_CMD 0xBA  		//写命令地址
-#define GT_RD_CMD 0xBB  		//写数据地址
+#define GT_RD_CMD 0xBB  		//读数据地址
 
 #define GT_Point_ADDR 0x814E	//GT911当前检测到的触摸情况，第七位是触摸标志位，低4位是触摸点数个数
 #define GT_ID_ADDR    0x8140	//GT911产品ID寄存器
@@ -32,6 +32,13 @@ typedef struct
 extern _m_tp_dev tp_dev;	 	//触屏控制器在touch.c里面定义
 
 
+#define CTP_INT_Clr() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET)//INT
+#define CTP_INT_Set() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET)
+
+#define CTP_RES_Clr() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET)//RES
+#define CTP_RES_Set() HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET)
+
+void CTP_GPIOInit(void);
 uint8_t GT_WR_DATA(uint16_t addr,uint8_t data);
 uint8_t GT_RD_DATA(uint16_t addr,uint8_t len,uint8_t *value);
 void  GT_Init(void);
@@ -44,7 +51,8 @@ void lcd_draw_bline(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,uint16_t 
 
 void GT911_test(void);
 
-void Button_Scan(void);
+void GT911_Write_Data(uint16_t addr,uint8_t data);
+void GT911_Read_Data(uint16_t addr,uint8_t len,uint8_t *value);
 
 #endif
 
